9537b50c7a1023cc85d2df6db8f8218afbc7d9ed
NightHawkNB <nipunbathiya1256@outlook.com>
2023-07-10 14:40:41 +0530

    P05 - Q01 Completed

--- lab_sheet_05/01.scala
+++ lab_sheet_05/01.scala
@@ -3,15 +3,14 @@ object q01 extends App{
     def gcd(a:Int, b:Int): Int = b match {
         case 0 => a
         case x if x>a => gcd(x,a)
-        case x => gcd(x, x%a)
+        case x => gcd(x, a%x)
     }
 
-    def isPrime(n:Int, temp:Int=2):Boolean = n match {
-        case x if(x == 2) => true
-        case x if(x%temp == 0) => false
-        case x if(x == temp) => true
-        case _ => isPrime(n, temp+1)
+    def isPrime(n:Int, temp:Int=2):Boolean = temp match {
+        case x if(x == n) => true
+        case x if gcd(n,x)>1 => false
+        case x => isPrime(n, x+1)
     }
 
-    println(isPrime(5))
+    println(isPrime(11))
 }
