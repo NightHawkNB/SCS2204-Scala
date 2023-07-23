d052b60ab953df93e2c9728ebfcee3c72cf96acb
NightHawkNB <nipunbathiya1256@outlook.com>
2023-07-23 17:57:44 +0530

    Fixes

--- lab_sheet_06/question.scala
+++ lab_sheet_06/question.scala
@@ -3,8 +3,28 @@ object cipher_code extends App{
     def encrypt(word: String, cipher:Int = 1): String = {
         var charArray = word.toCharArray();
         var output = ""
+        var letter_02 = 0
 
-        for(letter <- charArray) output = output.concat((letter+1).toChar.toString)
+        for(letter <- charArray) {
+            if(letter == 32) {
+                output = output.concat(" ")
+            } else if(letter >= 65 && letter <= 90) {
+                letter_02 = letter+cipher
+                if(letter_02 > 90) {
+                    output = output.concat((letter_02-26).toChar.toString)
+                }else{
+                    output = output.concat(letter_02.toChar.toString)
+                }
+            } else if(letter >= 96 && letter <= 122) {
+                letter_02 = letter+cipher
+                if(letter_02 > 122) {
+                    output = output.concat((letter_02-26).toChar.toString)
+                } else {
+                    output = output.concat(letter_02.toChar.toString)
+                }
+            }
+            
+        }
 
         output
     }
@@ -12,8 +32,28 @@ object cipher_code extends App{
     def decrypt(word: String, cipher:Int = 1): String = {
         var charArray = word.toCharArray();
         var output = ""
+        var letter_02 = 0
 
-        for(letter <- charArray) output = output.concat((letter-1).toChar.toString)
+        for(letter <- charArray) {
+            if(letter == 32) {
+                output = output.concat(" ")
+            }else if(letter >= 65 && letter <= 90) {
+                letter_02 = letter-cipher
+                if(letter_02 < 65) {
+                    output = output.concat((letter_02+26).toChar.toString)
+                } else {
+                    output = output.concat(letter_02.toChar.toString)
+                }
+            }   
+            else if(letter >= 96 && letter <= 122) {
+                letter_02 = letter*cipher
+                if(letter_02 < 96) {
+                    output = output.concat((letter_02+26).toChar.toString)
+                } else {
+                    output = output.concat(letter_02.toChar.toString)
+                }
+            }    
+        }
 
         output
     }
@@ -22,7 +62,7 @@ object cipher_code extends App{
         func(data, cipher)
     }
 
-    var data = "ABCD EFGH"
+    var data = "ABCD WXYZ ZZHX"
 
     println("Starting String : " + data)
 
